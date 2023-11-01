2313dc6b02508bfd898a232a36ec42bfd6504f2d
AkagawaTsurunaki <965145619@qq.com>
2023-11-01 13:09:19 +0800

    Init commit

---
+++ src/main/java/com/github/nanoyou/akariyumetabackend/AkariYumetaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.nanoyou.akariyumetabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkariYumetaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkariYumetaBackendApplication.class, args);
+	}
+
+}
