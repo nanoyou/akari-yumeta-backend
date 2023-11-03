5b1efac5e510994535ffbb3f039e085f7f5fabba
AkagawaTsurunaki <965145619@qq.com>
2023-11-03 12:07:13 +0800

    todo: TaskController, TaskService, CourseService 待实现

---
+++ src/main/java/com/github/nanoyou/akariyumetabackend/service/CourseService.java
@@ -0,0 +1,26 @@
+package com.github.nanoyou.akariyumetabackend.service;
+
+import com.github.nanoyou.akariyumetabackend.common.NotImplementedException;
+import com.github.nanoyou.akariyumetabackend.dao.CourseDao;
+import com.github.nanoyou.akariyumetabackend.entity.task.Course;
+import jakarta.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    private final CourseDao courseDao;
+
+    @Autowired
+    private CourseService(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
+    public Optional<Course> addCourse(@Nonnull Course course) {
+        // TODO: 请 flozxwer 完成这里
+        throw new NotImplementedException("flozxwer");
+    }
+}
