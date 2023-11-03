012dd4d361505dc8d24840985e116eb04edc3209
yuzilai <2557491185@qq.com>
2023-11-03 20:48:01 +0800

    统一返回格式

---
+++ src/main/java/com/github/nanoyou/common/ResponseBody.java
@@ -0,0 +1,30 @@
+package com.github.nanoyou.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+// 统一返回格式
+public class ResponseBody {
+    private ResponseCode code;
+    private String msg;
+    private Object data;
+
+    public static ResponseBody success(Object data) {
+        return ResponseBody.builder().code(ResponseCode.SUCCESS).msg("SUCCESS").data(data).build();
+    }
+
+    public static ResponseBody paramErr(Object data) {
+        return ResponseBody.builder().code(ResponseCode.PARAM_ERR).msg("PARAM_ERR").data(data).build();
+    }
+
+    public static ResponseBody loginRequire(Object data) {
+        return ResponseBody.builder().code(ResponseCode.LOGIN_REQUIRE).msg("LOGIN_REQUIRE").data(data).build();
+    }
+
+    public static ResponseBody unAutoAuthorized(Object data) {
+        return ResponseBody.builder().code(ResponseCode.UNAUTHORIZED).msg("UN_AUTOAUTHORIZE").data(data).build();
+    }
+
+}
--- src/main/java/com/github/nanoyou/entity/DonateGoods.java
+++ src/main/java/com/github/nanoyou/entity/DonateGoods.java
@@ -1,4 +1,4 @@
-package com.github.nanoyou.Entity;
+package com.github.nanoyou.entity;
 
 import lombok.Builder;
 import lombok.Data;
