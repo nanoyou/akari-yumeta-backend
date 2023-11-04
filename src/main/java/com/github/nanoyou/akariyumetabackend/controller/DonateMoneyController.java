9a92184afbb5c1b31d2c23a61eb5966e9d566e6e
yuzilai <2557491185@qq.com>
2023-11-04 16:30:42 +0800

    平衡进度

--- src/main/java/com/github/nanoyou/akariyumetabackend/controller/DonateMoneyController.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/controller/DonateMoneyController.java
@@ -1,6 +1,15 @@
 package com.github.nanoyou.akariyumetabackend.controller;
 
+import com.github.nanoyou.akariyumetabackend.common.enumeration.ResponseCode;
+import com.github.nanoyou.akariyumetabackend.entity.Result;
+import com.github.nanoyou.akariyumetabackend.entity.donate.DonateMoney;
 import com.github.nanoyou.akariyumetabackend.service.DonateMoneyService;
+import jakarta.servlet.http.HttpSession;
+import org.hibernate.bytecode.enhance.spi.interceptor.BytecodeInterceptorLogging_$logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class DonateMoneyController {
 
     private DonateMoneyService donateMoneyService;
+
+    @Autowired
+    public DonateMoneyController(DonateMoneyService donateMoneyService) {
+        this.donateMoneyService = donateMoneyService;
+    }
+
+    @PostMapping("/donate/money")
+    public Result donateMoney(@RequestBody DonateMoney donateMoney, HttpSession session) {
+
+        // int userId = (int) session.getAttribute()
+        if (donateMoney.getAmount() == null || donateMoney.getAmount() <= 0) {
+            return Result.builder()
+                   .ok(false)
+                   .message("捐赠金额不合法")
+                   .code(ResponseCode.PARAM_ERR.value)
+                   .build();
+        }
+        DonateMoney result = donateMoneyService.saveDonateMoney(donateMoney);
+        return Result.builder()
+                .ok(true)
+                .message("捐赠成功")
+                .code(ResponseCode.SUCCESS.value)
+                .data(result)
+                .build();
+    }
+
+
 }
--- src/main/java/com/github/nanoyou/akariyumetabackend/service/DonateMoneyService.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/service/DonateMoneyService.java
@@ -1,9 +1,23 @@
 package com.github.nanoyou.akariyumetabackend.service;
 
 import com.github.nanoyou.akariyumetabackend.dao.DonateMoneyDao;
+import com.github.nanoyou.akariyumetabackend.entity.donate.DonateMoney;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class DonateMoneyService {
     private DonateMoneyDao donateMoneyDao;
+
+    @Autowired
+    public DonateMoneyService(DonateMoneyDao donateMoneyDao) {
+        this.donateMoneyDao = donateMoneyDao;
+    }
+
+    public DonateMoney saveDonateMoney(DonateMoney donateMoney) {
+        return donateMoneyDao.save(donateMoney);
+    }
+
 }
