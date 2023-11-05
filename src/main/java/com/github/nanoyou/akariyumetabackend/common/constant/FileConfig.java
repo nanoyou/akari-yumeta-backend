879eafe4927a28ea5a2c39b6ffa7beda3d0b9bbb
AkagawaTsurunaki <965145619@qq.com>
2023-11-05 12:51:47 +0800

    refactor: 修改了文件上传大小限制

--- src/main/java/com/github/nanoyou/akariyumetabackend/common/constant/FileConfig.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/common/constant/FileConfig.java
@@ -1,5 +1,5 @@
 package com.github.nanoyou.akariyumetabackend.common.constant;
 
 public final class FileConfig {
-    public static final int MAX_FILE_BYTE_LENGTH = 10485760;
+    public static final int MAX_FILE_BYTE_LENGTH = 512 * 1024 * 1024;
 }
