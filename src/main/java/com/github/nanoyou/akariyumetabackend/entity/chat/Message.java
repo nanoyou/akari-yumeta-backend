5a9bf9e2bee1009482e53c383f3528ce06db87b3
AkagawaTsurunaki <965145619@qq.com>
2023-11-05 22:50:06 +0800

    feat: Message 表非空约束, UUID约束

--- src/main/java/com/github/nanoyou/akariyumetabackend/entity/chat/Message.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/entity/chat/Message.java
@@ -2,10 +2,13 @@ package com.github.nanoyou.akariyumetabackend.entity.chat;
 
 import com.github.nanoyou.akariyumetabackend.entity.enumeration.MessageType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UUID;
 
 import java.time.LocalDateTime;
 
@@ -28,26 +31,34 @@ public class Message {
     /**
      * 发送者 ID
      */
+    @UUID
+    @NotNull
     private String senderID;
     /**
      * 接收者 ID
      */
+    @UUID
+    @NotNull
     private String receiverID;
     /**
      * 消息内容
      */
+    @NotBlank
     private String content;
     /**
      * 是否已读
      */
+    @NotNull
     private boolean isRead;
     /**
      * 发送时间
      */
+    @NotNull
     private LocalDateTime sendTime;
     /**
      * 消息类型
      */
+    @NotNull
     @Enumerated(EnumType.STRING)
     private MessageType type;
 }
