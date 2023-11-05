380d2733f5733ac13c70cc0be1357535e4b6e4ae
AkagawaTsurunaki <965145619@qq.com>
2023-11-05 22:37:01 +0800

    feat: Course 表非空约束, 自然数约束, UUID约束, URL约束

--- src/main/java/com/github/nanoyou/akariyumetabackend/entity/task/Course.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/entity/task/Course.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+import org.hibernate.validator.constraints.UUID;
 
 /**
  * Course 课程
@@ -24,19 +28,27 @@ public class Course {
      * 关联的Task的ID
      */
     @Id
+    @NotNull
+    @UUID
     private String taskID;
     /**
      * 被观看的次数
      */
+    @NotNull
+    @Min(value = 0)
     private Integer watchedCount;
     /**
      * 视频地址
      * 要给受困儿童显示的视频的地址
      */
+    @NotNull
+    @URL
     private String videoURL;
     /**
      * 视频时长
      * 单位秒，注意视频时长不等于任务时长
      */
+    @NotNull
+    @Min(value = 0)
     private Integer videoDuration;
 }
