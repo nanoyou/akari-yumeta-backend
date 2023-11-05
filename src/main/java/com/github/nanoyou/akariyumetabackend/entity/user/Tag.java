2546fc9ece93f5adc4bb027981e0f1a981a794d7
AkagawaTsurunaki <965145619@qq.com>
2023-11-05 22:31:00 +0800

    feat: Tag 表非空约束

--- src/main/java/com/github/nanoyou/akariyumetabackend/entity/user/Tag.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/entity/user/Tag.java
@@ -3,6 +3,7 @@ package com.github.nanoyou.akariyumetabackend.entity.user;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,8 +27,11 @@ public class Tag {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class CombinedPrimaryKey implements Serializable {
+
+        @NotNull
         private String userId;
 
+        @NotNull
         private String tagContent;
     }
 }
