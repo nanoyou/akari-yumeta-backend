0c8c945d56a8eb096ce0afaa1eef0891361d8f76
AkagawaTsurunaki <965145619@qq.com>
2023-11-07 10:02:08 +0800

    feat: 插入一些数据

--- src/main/java/com/github/nanoyou/akariyumetabackend/common/config/InterceptorConfig.java
+++ src/main/java/com/github/nanoyou/akariyumetabackend/common/config/InterceptorConfig.java
@@ -40,7 +40,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login/admin")
                 .excludePathPatterns("/login")
-                .excludePathPatterns("/register");
+                .excludePathPatterns("/register")
+                .excludePathPatterns("/file/*");
 
         // 在下面添加需要 管理员 登录的路径，需要添加至上方 loginInterceptor 内
         registry.addInterceptor(adminInterceptor)
